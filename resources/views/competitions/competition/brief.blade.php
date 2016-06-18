f0720c7ca43cda0facdc73f3d850ef27edd06981
Daniel Walder <daniel.walder@gmail.com>
2016-06-18 15:06:06 +1000

    Competition results page

--- resources/views/competitions/competition/brief.blade.php
+++ resources/views/competitions/competition/brief.blade.php
@@ -107,6 +107,102 @@
             @include('competitions.entry._entry-form-fields', [ 'comp' => $comp, 'entry' => $user_entry ])
         @endform
     @endif
+    @if ($comp->isClosed())
+
+        <hc>
+            <h1>
+                Results
+                @if ($comp->isJudged() && $comp->judges->count() > 0)
+                    <small class="pull-right">
+                        Judged By:
+                        {? $i = 0; ?}
+                        @foreach ($comp->judges as $judge)
+                            {!! $i++ == 0 ? '' : ' &bull; ' !!}
+                            @avatar($judge inline)
+                        @endforeach
+                    </small>
+                @endif
+            </h1>
+        </hc>
+
+
+        @if ($comp->results_intro_html)
+            <div class="bbcode">{!! $comp->results_intro_html !!}</div>
+        @endif
+        <ul class="media-list">
+            {? $prev_rank = -1; ?}
+            @foreach ($comp->getEntriesForResults() as $entry)
+                {? $result = $comp->results->where('entry_id', $entry->id)->first(); ?}
+                @if ($prev_rank != 0 && $result->rank == 0)
+                    </ul>
+                    <hr/>
+                    <h3>Other Entries</h3>
+                    <ul class="media-list">
+                @endif
+                {? $shot = $entry->screenshots->first(); ?}
+                {? $prev_rank = $result->rank; ?}
+                <li class="media media-panel" data-id="{{ $entry->id }}">
+                    <div class="media-heading">
+                        {? $result = $comp->results->where('entry_id', $entry->id)->first(); ?}
+                        @if ($result->rank == 1)
+                            <h2>1st Place</h2>
+                        @elseif ($result->rank == 2)
+                            <h2>2nd Place</h2>
+                        @elseif ($result->rank == 3)
+                            <h2>3rd Place</h2>
+                        @endif
+                        <h3>{{ $entry->title }}</h3>
+                        <h5>
+                            By @avatar($entry->user inline)
+                            @if ($entry->file_location)
+                                <a href="{{ $entry->getLinkUrl() }}" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-download-alt"></span> Download</a>
+                            @endif
+                        </h5>
+                    </div>
+                    <div class="media-body">
+                        <div class="visible-sm-block visible-xs-block text-center">
+                            <div style="display: inline-block;">
+                                <a href="#" class="gallery-button img-thumbnail">
+                                    @if ($shot)
+                                        <img class="media-object" src="{{ asset('uploads/competition/'.$shot->image_thumb) }}" alt="Screenshot" />
+                                    @else
+                                        <img class="media-object" src="{{ asset('images/no-screenshot-320.png') }}" alt="Screenshot" />
+                                    @endif
+                                </a>
+                                @if ($entry->screenshots->count() > 1)
+                                    <button class="btn btn-info btn-block gallery-button" type="button">
+                                        <span class="glyphicon glyphicon-picture"></span>
+                                        + {{ $entry->screenshots->count()-1 }} more screenshot{{ $entry->screenshots->count() == 2 ? '' : 's' }}
+                                    </button>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="bbcode">{!! $result->content_html !!}</div>
+                    </div>
+                    <div class="media-right hidden-xs hidden-sm">
+                        <a href="#" class="gallery-button img-thumbnail">
+                            @if ($shot)
+                                <img class="media-object" src="{{ asset('uploads/competition/'.$shot->image_thumb) }}" alt="Screenshot" />
+                            @else
+                                <img class="media-object" src="{{ asset('images/no-screenshot-320.png') }}" alt="Screenshot" />
+                            @endif
+                        </a>
+                        @if ($entry->screenshots->count() > 1)
+                            <button class="btn btn-info btn-block gallery-button" type="button">
+                                <span class="glyphicon glyphicon-picture"></span>
+                                + {{ $entry->screenshots->count()-1 }} more screenshot{{ $entry->screenshots->count() == 2 ? '' : 's' }}
+                            </button>
+                        @endif
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        @if ($comp->results_outro_html)
+            <div class="bbcode">{!! $comp->results_outro_html !!}</div>
+        @endif
+
+
+    @endif
 @endsection
 
 @section('scripts')
