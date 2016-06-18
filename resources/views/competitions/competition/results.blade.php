07e60923e99a37eb6fc313c47ac318c6f1b11c48
Daniel Walder <daniel.walder@gmail.com>
2016-06-18 14:44:22 +1000

    Competition results page

--- resources/views/competitions/competition/results.blade.php
+++ resources/views/competitions/competition/results.blade.php
@@ -41,48 +41,57 @@
             {? $shot = $entry->screenshots->first(); ?}
             {? $prev_rank = $result->rank; ?}
             <li class="media" data-id="{{ $entry->id }}">
-                <div class="media-body">
-                    <div class="media-heading">
-                        {? $result = $comp->results->where('entry_id', $entry->id)->first(); ?}
-                        @if ($result->rank == 1)
-                            <h2>1st Place</h2>
-                        @elseif ($result->rank == 2)
-                            <h2>2nd Place</h2>
-                        @elseif ($result->rank == 3)
-                            <h2>3rd Place</h2>
+                <div class="media-heading">
+                    {? $result = $comp->results->where('entry_id', $entry->id)->first(); ?}
+                    @if ($result->rank == 1)
+                        <h2>1st Place</h2>
+                    @elseif ($result->rank == 2)
+                        <h2>2nd Place</h2>
+                    @elseif ($result->rank == 3)
+                        <h2>3rd Place</h2>
+                    @endif
+                    <h3>{{ $entry->title }}</h3>
+                    <h5>
+                        By @avatar($entry->user inline)
+                        @if ($entry->file_location)
+                            <a href="{{ $entry->getLinkUrl() }}" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-download-alt"></span> Download</a>
                         @endif
-                        <h3>
-                            {{ $entry->title }}
-                        </h3>
-                        <h4>
-                            By @avatar($entry->user inline)
-                            @if ($entry->file_location)
-                                <a href="{{ $entry->getLinkUrl() }}" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-download-alt"></span> Download</a>
-                            @endif
-                        </h4>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-5 col-md-push-7 text-center">
-                            <div style="display: inline-block;">
-                                <a href="#" class="gallery-button img-thumbnail">
-                                    @if ($shot)
-                                        <img class="media-object" src="{{ asset('uploads/competition/'.$shot->image_thumb) }}" alt="Screenshot" />
-                                    @else
-                                        <img class="media-object" src="{{ asset('images/no-screenshot-320.png') }}" alt="Screenshot" />
-                                    @endif
-                                </a>
-                                @if ($entry->screenshots->count() > 1)
-                                    <button class="btn btn-info btn-block gallery-button" type="button">
-                                        <span class="glyphicon glyphicon-picture"></span>
-                                        + {{ $entry->screenshots->count()-1 }} more screenshot{{ $entry->screenshots->count() == 2 ? '' : 's' }}
-                                    </button>
+                    </h5>
+                </div>
+                <div class="media-body">
+                    <div class="visible-sm-block visible-xs-block text-center">
+                        <div style="display: inline-block;">
+                            <a href="#" class="gallery-button img-thumbnail">
+                                @if ($shot)
+                                    <img class="media-object" src="{{ asset('uploads/competition/'.$shot->image_thumb) }}" alt="Screenshot" />
+                                @else
+                                    <img class="media-object" src="{{ asset('images/no-screenshot-320.png') }}" alt="Screenshot" />
                                 @endif
-                            </div>
-                        </div>
-                        <div class="col-md-pull-5 col-md-7">
-                            <div class="bbcode">{!! $result->content_html !!}</div>
+                            </a>
+                            @if ($entry->screenshots->count() > 1)
+                                <button class="btn btn-info btn-block gallery-button" type="button">
+                                    <span class="glyphicon glyphicon-picture"></span>
+                                    + {{ $entry->screenshots->count()-1 }} more screenshot{{ $entry->screenshots->count() == 2 ? '' : 's' }}
+                                </button>
+                            @endif
                         </div>
                     </div>
+                    <div class="bbcode">{!! $result->content_html !!}</div>
+                </div>
+                <div class="media-right hidden-xs hidden-sm">
+                    <a href="#" class="gallery-button img-thumbnail">
+                        @if ($shot)
+                            <img class="media-object" src="{{ asset('uploads/competition/'.$shot->image_thumb) }}" alt="Screenshot" />
+                        @else
+                            <img class="media-object" src="{{ asset('images/no-screenshot-320.png') }}" alt="Screenshot" />
+                        @endif
+                    </a>
+                    @if ($entry->screenshots->count() > 1)
+                        <button class="btn btn-info btn-block gallery-button" type="button">
+                            <span class="glyphicon glyphicon-picture"></span>
+                            + {{ $entry->screenshots->count()-1 }} more screenshot{{ $entry->screenshots->count() == 2 ? '' : 's' }}
+                        </button>
+                    @endif
                 </div>
             </li>
         @endforeach
